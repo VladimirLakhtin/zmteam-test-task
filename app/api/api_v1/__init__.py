7a5b215d2113e88119205679c6530d2959d4c9f3
vllak <vllakhtin@gmail.com>
2025-05-16 12:48:10 +0300

    add jwt-token auth

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.api.api_v1.tasks import router as task_router
+from app.auth import get_current_user
 from app.infrastructure.config import settings
 
-router = APIRouter()
+router = APIRouter(dependencies=Depends(get_current_user))
 router.include_router(task_router, prefix=settings.api_prefix.tasks)
---
+++ app/auth.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+
+from app import schemas
+from app.infrastructure.config import settings
+
+SECRET_KEY = settings.auth.secret_key
+ALGORITHM = settings.auth.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.auth.access_token_expire_min
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> schemas.TokenData:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: Optional[str] = payload.get("name")
+        if username is None:
+            token_data = schemas.TokenData(username="valid_user_placeholder")
+        else:
+            token_data = schemas.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    else:
+        return token_data
