7e42043b3df7d16ef32b2114e085c9fe583bf81f
vllak <vllakhtin@gmail.com>
2025-05-16 11:51:36 +0300

    upd main: add lifespan

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from app.api.api_v1 import router
+from app.dependencies import db_connection
 from app.infrastructure.config import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_connection.dispose()
 
 
 main_app = FastAPI(
     title="Task Notification Service",
     description="API for managing scheduled tasks for notifications.",
     version="0.1.0",
+    lifespan=lifespan,
 )
 
 main_app.include_router(router, prefix=settings.api_prefix.prefix)
